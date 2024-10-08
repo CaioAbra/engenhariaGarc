44cd6ceb88854c323e4aeb48d819e4fc434eb7d8
CaioAbra <caioabra41@gmail.com>
2024-10-08 15:15:57 -0300

    ajuste de semantica

--- controllers/QuemSomosController.php
+++ controllers/QuemSomosController.php
@@ -7,6 +7,6 @@ class QuemSomosController {
         // por exemplo, buscar dados do banco de dados e passá-los para a visualização
         
         // Inclua a visualização correspondente
-        require_once 'views/quemsomos.php';
+        require_once 'views/quemSomos.php';
     }
 }
