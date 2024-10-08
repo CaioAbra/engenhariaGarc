3eb5c108555a5c20d60ab44692152008fb21efcb
CaioAbra <caioabra41@gmail.com>
2024-10-08 01:16:54 -0300

    ajuste script

--- views/components/insertScripts.php
+++ views/components/insertScripts.php
@@ -1,17 +1,13 @@
 <?php
-// Obtém o caminho da URL
-$url_path = $_SERVER['REQUEST_URI'];
-// Divide a URL em partes usando "/"
-$path_parts = explode('/', trim($url_path, '/'));
-// Dependendo do número de partes no caminho, você define diferentes caminhos
-if (count($path_parts) >= 3) {
-    $base_path = "../assets/";
+// Verifica se o ambiente é localhost ou servidor de produção
+if (strpos($_SERVER['HTTP_HOST'], 'localhost') !== false) {
+    $base_path = './assets/';
 } else {
-    $base_path = "./assets/";
+    $base_path = '../assets/';
 }
 ?>
 <script src="<?php echo $base_path . 'lib/jquery-3.7.1.min.js'; ?>"></script>
-<script src="<?php echo $base_path . 'lib/bootstrap-4.6.2-dist/js/bootstrap.bundle.min'; ?>"></script>
+<script src="<?php echo $base_path . 'lib/bootstrap-4.6.2-dist/js/bootstrap.bundle.min.js'; ?>"></script>
 <script src="<?php echo $base_path . 'lib/bootstrap-4.6.2-dist/js/bootstrap.min.js'; ?>"></script>
 <script src="<?php echo $base_path . 'lib/popper.min.js'; ?>"></script>
 <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
