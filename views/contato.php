9313bf98463ecebb48c43931cb5ef9e7583bed35
CaioAbra <caioabra41@gmail.com>
2024-10-08 00:52:08 -0300

    contato

--- views/contato.php
+++ views/contato.php
@@ -12,200 +12,84 @@
 <body>
     <?php include 'components/navbar.php'; ?>
 
-    <main class="contato">
-        <section class="banner banner-contato">
-            <div class="container">
-                <div class="row align-content-center">
-                    <div class="col-12">
-                        <h6 class="banner-subtitle">Solicite um orçamento</h6>
-                        <h1 class="banner-title">Entre em contato com a GARC</h1>
-                    </div>
-                </div>
-            </div>
-        </section>
+    <body>
 
-        <section>
-            <div class="container">
-                <div class="row">
-                    <div class="col-12 col-md-6 col-lg-4">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">Numeros de Celular</h5>
-                                <p class="card-text">
-                                    <a href="tel:11994908731">11 99490-8731</a>
-                                </p>
-                            </div>
+        <main class="contato">
+            <!-- Seção de Informações de Contato -->
+            <section class="info-contato bg-light py-5">
+                <div class="container text-center">
+                    <h1 class="mb-4">Entre em Contato</h1>
+                    <p class="lead">Entre em contato com a Engenharia GARC para solicitar um orçamento ou tirar dúvidas sobre nossos serviços.</p>
+                    <div class="row mt-5">
+                        <div class="col-md-4">
+                            <i class="fas fa-phone fa-2x mb-3"></i>
+                            <h5>Telefone</h5>
+                            <p><a href="tel:11941262774">11 94126-2774</a><br><a href="tel:11941262774">11 94019-7369</a></p>
                         </div>
-                    </div>
-
-                    <div class="col-12 col-md-6 col-lg-4">
-                        <div class="card">
-                            <div class="card-body pb-1">
-                                <h5 class="card-title">E-mail</h5>
-                                <p class="card-text">
-                                    <a href="mail-to:engenhariagarc@engenhariagarc.com">
-                                        engenhariagarc@engenhariagarc.com
-                                    </a>
-                                </p>
-                            </div>
+                        <div class="col-md-4">
+                            <i class="fas fa-envelope fa-2x mb-3"></i>
+                            <h5>E-mail</h5>
+                            <p><a href="mailto:engenhariagarc@engenhariagarc.com">engenhariagarc@engenhariagarc.com</a></p>
                         </div>
-                    </div>
-
-                    <div class="col-12 col-md-6 col-lg-4">
-                        <div class="card">
-                            <div class="card-body pb-1">
-                                <h5 class="card-title">Hora de atendimento</h5>
-                                <div class="card-text">
-                                    <p>Seg: 8h às 18h</p>
-                                    <p>Ter: 8h às 18h</p>
-                                    <p>Qua: 8h às 18h</p>
-                                    <p>Qui: 8h às 18h</p>
-                                    <p>Sex: 8h às 18h</p>
-                                    <p>Sab: Fechado</p>
-                                    <p>Dom: Fechado</p>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        <section class="perguntasFrequentes">
-            <div class="container">
-                <div class="row text-align-center">
-                    <div class="col-12">
-                        <h4>
-                            Duvidas Frequentes
-                        </h4>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-12 col-md-6">
-                        <div class="accordion">
-                            <div class="accordion-item">
-                                <div class="accordion-header">
-                                    Quais serviços a GARC Engenharia oferece?
-                                </div>
-                                <div class="accordion-content">
-                                    A GARC Engenharia oferece uma ampla gama de serviços em engenharia civil,
-                                    elétrica e solar, incluindo projetos de construção, instalações elétricas,
-                                    sistemas de energia solar, consultoria e gerenciamento de projetos, entre
-                                    outros.
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-12 col-md-6">
-                        <div class="accordion">
-                            <div class="accordion-item">
-                                <div class="accordion-header">
-                                    Como posso solicitar um orçamento para um projeto?
-                                </div>
-                                <div class="accordion-content">
-                                    Para solicitar um orçamento para um projeto específico, você pode entrar em
-                                    contato conosco através do formulário de contato em nosso site, por e-mail ou
-                                    telefone. Por favor, forneça o máximo de detalhes possível sobre o projeto para
-                                    que possamos fornecer um orçamento preciso.
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-12 col-md-6">
-                        <div class="accordion">
-                            <div class="accordion-item">
-                                <div class="accordion-header">
-                                    Quais informações devo fornecer ao solicitar um orçamento?
-                                </div>
-                                <div class="accordion-content">
-                                    Ao solicitar um orçamento, é útil fornecer informações como o tipo de serviço
-                                    necessário, as dimensões do projeto, a localização, os prazos e quaisquer
-                                    requisitos específicos. Quanto mais detalhes você puder fornecer, melhor
-                                    poderemos atender às suas necessidades.
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-12 col-md-6">
-                        <div class="accordion">
-                            <div class="accordion-item">
-                                <div class="accordion-header">
-                                    Qual é o processo de trabalho da GARC Engenharia?
-                                </div>
-                                <div class="accordion-content">
-                                    Nosso processo de trabalho geralmente inclui uma consulta inicial para entender
-                                    os requisitos do projeto, seguida pelo desenvolvimento de um plano detalhado e a
-                                    execução do projeto em conformidade com os mais altos padrões de qualidade e
-                                    segurança.
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-12 col-md-6">
-                        <div class="accordion">
-                            <div class="accordion-item">
-                                <div class="accordion-header">
-                                    A GARC Engenharia oferece serviços personalizados?
-                                </div>
-                                <div class="accordion-content">
-                                    Sim, estamos sempre dispostos a adaptar nossos serviços às necessidades
-                                    específicas de nossos clientes. Se você precisar de algo além do padrão, entre
-                                    em contato conosco para discutir suas necessidades e desenvolver uma solução sob
-                                    medida para você.
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-12 col-md-6">
-                        <div class="accordion">
-                            <div class="accordion-item">
-                                <div class="accordion-header">
-                                    Quais são os prazos típicos para a conclusão de um projeto?
-                                </div>
-                                <div class="accordion-content">
-                                    Os prazos de conclusão variam de acordo com a complexidade e o escopo do
-                                    projeto. Após uma análise inicial, podemos fornecer uma estimativa dos prazos de
-                                    conclusão para o seu projeto específico.
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-12 col-md-6">
-                        <div class="accordion">
-                            <div class="accordion-item">
-                                <div class="accordion-header">
-                                    A GARC Engenharia oferece garantia para seus serviços?
-                                </div>
-                                <div class="accordion-content">
-                                    Sim, oferecemos garantia para nossos serviços, garantindo qualidade, eficiência
-                                    e segurança em cada etapa do projeto. Entre em contato conosco para saber mais
-                                    sobre nossas políticas de garantia.
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-12 col-md-6">
-                        <div class="accordion">
-                            <div class="accordion-item">
-                                <div class="accordion-header">
-                                    A GARC Engenharia está disponível para projetos de grande escala?
-                                </div>
-                                <div class="accordion-content">
-                                    Sim, estamos equipados para lidar com projetos de todos os tamanhos, desde
-                                    pequenas instalações residenciais até grandes empreendimentos comerciais e
-                                    industriais. Entre em contato conosco para discutir os detalhes do seu projeto.
-                                </div>
-                            </div>
+                        <div class="col-md-4">
+                            <i class="fas fa-map-marker-alt fa-2x mb-3"></i>
+                            <h5>Localização</h5>
+                            <p>Caieiras, SP</p>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
-    </main>
+            </section>
 
-    <footer class="">
+            <!-- Seção de Formulário de Contato -->
+            <section class="formulario-contato py-5">
+                <div class="container">
+                    <h2 class="text-center mb-4">Envie uma Mensagem</h2>
+                    <form action="envia_email.php" method="POST" class="form-contato">
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                <label for="nome">Nome</label>
+                                <input type="text" class="form-control" id="nome" name="nome" placeholder="Seu nome" required>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="email">E-mail</label>
+                                <input type="email" class="form-control" id="email" name="email" placeholder="Seu e-mail" required>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="mensagem">Mensagem</label>
+                            <textarea class="form-control" id="mensagem" name="mensagem" rows="5" placeholder="Escreva sua mensagem aqui..." required></textarea>
+                        </div>
+                        <div class="text-center mt-4">
+                            <button type="submit" class="btn btn-primary">Enviar Mensagem</button>
+                        </div>
+                    </form>
+                </div>
+            </section>
+
+            <!-- Seção de Mapa de Localização -->
+            <section class="mapa-localizacao py-5">
+                <div class="container text-center">
+                    <h2 class="mb-4">Nossa Localização</h2>
+                    <div class="mapa">
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3658.409688931204!2d-46.74373628493554!3d-23.364546084774422!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ceeb33ab8e36bb%3A0x4784049a4e024440!2sCaieiras%2C%20SP%2C%2007713-605!5e0!3m2!1spt-BR!2sbr!4v1696807064568!5m2!1spt-BR!2sbr"
+                            width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen>
+                        </iframe>
+                    </div>
+                </div>
+            </section>
+
+
+
+            <!-- Seção de Chamada para Ação -->
+            <section class="chamada-contato text-center text-light bg-secondary py-5">
+                <h2>Pronto para começar seu projeto?</h2>
+                <a href="mailto:engenhariagarc@engenhariagarc.com" class="btn btn-light mt-3">Fale Conosco Agora</a>
+            </section>
+        </main>
+
+
+    <footer class="mt-5">
         <?php include 'components/footer.php'; ?>
     </footer>
 
